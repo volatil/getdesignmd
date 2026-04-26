068f9194271fa75d9da3a3f23aaf1acdce5cd091
Paulo Torrealba <3134501+volatil@users.noreply.github.com>
2026-04-26 13:23:41 -0400

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium"]
+  }
 };
 
 export default nextConfig;
